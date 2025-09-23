85ebb84c9315b5956f03fd9d876ff206c6d937cd
Ahmed Salah Fathy Sotohy <laslsalsa123@gmail.com>
2025-09-23 10:09:52 +0300

    feat(email): implement email service, EMAIL MODULE SETUP and Install required dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule],
+  imports: [PrismaModule, AuthModule, UsersModule, EmailModule],
   controllers: [AppController],
   providers: [AppService],
 })

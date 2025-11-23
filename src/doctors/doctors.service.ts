5923ec68f2412726029b0830d40da8e6fd81a51c
Ahmed Salah Fathy Sotohy <laslsalsa123@gmail.com>
2025-11-23 02:27:01 +0200

    chore: remove doctors module
     Removed doctors controller, service, and related DTOs as they are no longer needed.

--- src/doctors/doctors.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { CreateDoctorDto } from './dto/create-doctor.dto';
-import { UpdateDoctorDto } from './dto/update-doctor.dto';
-
-@Injectable()
-export class DoctorsService {}
